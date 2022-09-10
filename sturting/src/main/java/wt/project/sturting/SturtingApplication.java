a5f522da9d5ab8e7053fb3fb9dd38f3a35bd77fe
xi_jjun <rlawowns97@naver.com>
2022-09-10 15:20:06 +0900

    project init

---
+++ sturting/src/main/java/wt/project/sturting/SturtingApplication.java
@@ -0,0 +1,13 @@
+package wt.project.sturting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SturtingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SturtingApplication.class, args);
+	}
+
+}
---
+++ sturting/src/test/java/wt/project/sturting/SturtingApplicationTests.java
@@ -0,0 +1,13 @@
+package wt.project.sturting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SturtingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
